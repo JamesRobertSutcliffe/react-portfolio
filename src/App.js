9bc43229dbb1effc65cdc35c1f359e40d5e2b00b
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-03-10 20:05:31 +0000

    First pages developed - think about layout - keep simple

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Nav from './Components/Nav';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Nav />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Nav = () => {
+    return <div class="d-flex align-items-center">
+        <nav class="navbar navbar-light m-2 p-2">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="https://www.feednavigator.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/feed/feednavigator.com/article/2021/11/30/dsm-to-acquire-norwegian-producer-of-pea-derived-protein-starch-and-fiber/13051660-1-eng-GB/DSM-to-acquire-Norwegian-producer-of-pea-derived-protein-starch-and-fiber.jpg" alt="" height="50" class="d-inline-block align-text-top"></img>
+
+                </a>
+            </div>
+        </nav>
+    </div>
+}
+
+export default Nav;
