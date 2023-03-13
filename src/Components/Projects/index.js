bc9c9e886e3cbb7d27261a7c988a12986e94bd03
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-03-13 19:58:08 +0000

    scalable project section developed

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Project from "./project";
+import projectData from "./../../projectData.json"
 
 
 const Projects = () => {
@@ -7,15 +8,13 @@ const Projects = () => {
     return (
         <div class="container mt-2">
             <div class="row">
-                <div class="col-lg-4 col-sm-12">
-                    <Project />
-                </div>
-                <div class="col-lg-4 col-sm-12">
-                    <Project />
-                </div>
-                <div class="col-lg-4 col-sm-12">
-                    <Project />
-                </div>
+                {projectData.map(item => {
+                    return <Project name={item.name}
+                        image={item.image}
+                        stack1={item.stack[0]}
+                        stack2={item.stack[1]}
+                        stack3={item.stack[2]} />
+                })}
             </div>
         </div>
     );
